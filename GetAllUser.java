58d4ac7d2cec730747df84d66b06f3d3507227d2
sab8313 <ouennes.sabry@gmail.com>
2024-12-16 10:46:33 +0100

    RouteTicketApp
    GetOneTicket et GetAllTicket ont deja les methode en plus des routes

---
+++ GetAllUser.java
@@ -0,0 +1,16 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/GetAllUser") // La route principale
+public class GetAllUser {
+
+    @GET // Méthode temporaire pour rendre la route visible
+    @Produces(MediaType.TEXT_PLAIN)
+    public String placeholder() {
+        return "Cette route est en cours de développement.";
+    }
+}
