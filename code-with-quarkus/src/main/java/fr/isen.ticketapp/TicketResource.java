efdf10c89929482d811ba24c0c9cc8a197b7a99e
Biiskote <rotyrobin886@gmail.com>
2024-12-19 11:13:17 +0100

    GetTicketById Fonctionnel

--- code-with-quarkus/src/main/java/fr/isen.ticketapp/TicketResource.java
+++ code-with-quarkus/src/main/java/fr/isen.ticketapp/TicketResource.java
@@ -1,16 +1,36 @@
 package fr.isen.ticketapp;
 
+import fr.isen.ticketapp.impl.services.TicketServiceImpl;
+import fr.isen.ticketapp.interfaces.models.TicketModel;
+import fr.isen.ticketapp.interfaces.services.TicketService;
+import io.smallrye.mutiny.Uni;
+import jakarta.json.Json;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
+import javax.print.DocFlavor;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @Path("/ticket")
 public class TicketResource {
+
+    // Méthode pour lire un fichier JSON
+    private String readFromJsonFile(String filePath) throws IOException {
+        // Utilise java.nio.file pour lire le fichier
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    private TicketService ticketService;
+
+    public TicketResource() {
+        this.ticketService = new TicketServiceImpl();
+    }
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -25,9 +45,12 @@ public class TicketResource {
         }
     }
 
-    // Méthode pour lire un fichier JSON
-    private String readFromJsonFile(String filePath) throws IOException {
-        // Utilise java.nio.file pour lire le fichier
-        return new String(Files.readAllBytes(Paths.get(filePath)));
+    @GET
+    @Path("/{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Uni<Response> getOneTicket(int id) {
+        TicketModel ticketModel = this.ticketService.getTicketById(id);
+        return Uni.createFrom().item(Response.ok(ticketModel).build());
     }
+
 }
---
+++ code-with-quarkus/src/main/java/fr/isen.ticketapp/impl.services/TicketServiceImpl.java
@@ -0,0 +1,38 @@
+package fr.isen.ticketapp.impl.services;
+
+import fr.isen.ticketapp.interfaces.models.TicketModel;
+import fr.isen.ticketapp.interfaces.models.enums.IMPACT;
+import fr.isen.ticketapp.interfaces.services.TicketService;
+
+import java.util.List;
+
+public class TicketServiceImpl implements TicketService {
+
+    @Override
+    public List<TicketModel> getTickets() {
+        return null;
+    }
+
+    @Override
+    public TicketModel getTicketById(int id) {
+        TicketModel ticket = new TicketModel();
+        ticket.titre = "TEST";
+        ticket.impact = IMPACT.Bloquant;
+        return ticket;
+    }
+
+    @Override
+    public TicketModel addTicket(TicketModel ticket) {
+        return null;
+    }
+
+    @Override
+    public TicketModel removeTicket(int id) {
+        return null;
+    }
+
+    @Override
+    public Integer updateTicket(TicketModel ticket) {
+        return 0;
+    }
+}
